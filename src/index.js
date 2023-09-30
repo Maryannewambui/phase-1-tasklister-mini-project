aecab1791ca11622afdbf11a35baeefaee24651a
Maryannewambui <trimbleleewambui@gmail.com>
2023-10-01 00:10:51 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+
+form.addEventListener("submit", e => {
+  //prevent the form from refreshing
+e.preventDefault(); 
+let taskDescription = document.getElementById("new-task-description").value;
+
+let listItems = document.createElement("li");
+listItems.textContent = taskDescription;
+taskList.appendChild(listItems);
+
+document.getElementById("new-task-description").value = "";
+})
+
 });
